6702a2d8ae7e1af6e0ab11ca0baa0f066d9b81ca
DamianKursa <damian.kursa.web@gmail.com>
2024-10-26 00:41:22 +0200

    fixing error

--- src/components/Product/FiltersControls.tsx
+++ src/components/Product/FiltersControls.tsx
@@ -1,4 +1,3 @@
-// FiltersControls.tsx
 import React from 'react';
 
 interface FiltersControlsProps {
@@ -10,6 +9,7 @@ interface FiltersControlsProps {
   onRemoveFilter: (filter: { name: string; value: string }) => void;
   isArrowDown: boolean;
   setIsArrowDown: React.Dispatch<React.SetStateAction<boolean>>;
+  isMobile: boolean; // Ensure this is defined here
 }
 
 const FiltersControls: React.FC<FiltersControlsProps> = ({
@@ -21,48 +21,38 @@ const FiltersControls: React.FC<FiltersControlsProps> = ({
   onRemoveFilter,
   isArrowDown,
   setIsArrowDown,
+  isMobile,
 }) => (
-  <div className="flex justify-between items-center mb-4">
+  <div className={`flex ${isMobile ? 'justify-center' : 'justify-between'} items-center mb-4`}>
     {/* Filtry Button */}
     <button
       onClick={toggleFilters}
-      className="filters-toggle border rounded-[24px] w-[352px] text-[24px] p-[7px_16px] flex justify-between items-center cursor-pointer"
+      className={`filters-toggle border rounded-[24px] ${isMobile ? 'w-1/2 text-[18px]' : 'w-[352px] text-[24px]'} p-[7px_16px] flex justify-center items-center cursor-pointer ${isMobile ? '' : 'justify-between'}`}
     >
-      <span className="font-semibold">Filtry</span>
-      <img
-        src={filtersVisible ? '/icons/arrow-left-black.svg' : '/icons/arrow-right-black.svg'}
-        alt="Toggle Filters"
-        className="w-[24px] h-[24px]"
-      />
+      <span className="font-semibold text-center">Filtry</span>
+      {!isMobile && (
+        <img
+          src={filtersVisible ? '/icons/arrow-left-black.svg' : '/icons/arrow-right-black.svg'}
+          alt="Toggle Filters"
+          className="w-[24px] h-[24px]"
+        />
+      )}
     </button>
 
-    {/* Active Filters */}
-    <div className="flex gap-2">
-      {filters.map((filter) => (
-        <span key={`${filter.name}-${filter.value}`} className="flex items-center text-[14px]">
-          {filter.value}
-          <button
-            onClick={() => onRemoveFilter(filter)}
-            className="ml-1 text-gray-500 hover:text-red-500"
-          >
-            &times;
-          </button>
-        </span>
-      ))}
-    </div>
-
     {/* Sorting Button */}
-    <div className="relative w-[340px]">
+    <div className={`relative ${isMobile ? 'w-1/2' : 'w-[340px]'}`}>
       <button
-        className="border rounded-[24px] w-[340px] text-[16px] p-[12px_20px] font-bold flex justify-between items-center"
+        className={`border rounded-[24px] ${isMobile ? 'w-full text-[18px]' : 'w-[340px] text-[16px]'} p-[7px_16px] font-bold flex justify-center items-center`}
         onClick={() => setIsArrowDown(!isArrowDown)}
       >
         Sortowanie
-        <img
-          src={isArrowDown ? '/icons/arrow-down.svg' : '/icons/arrow-up.svg'}
-          alt="Arrow"
-          className="w-[16px] h-[16px]"
-        />
+        {!isMobile && (
+          <img
+            src={isArrowDown ? '/icons/arrow-down.svg' : '/icons/arrow-up.svg'}
+            alt="Arrow"
+            className="w-[16px] h-[16px]"
+          />
+        )}
       </button>
       <select
         className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer z-10"
@@ -75,6 +65,23 @@ const FiltersControls: React.FC<FiltersControlsProps> = ({
         <option value="price-asc">Najniższa cena</option>
       </select>
     </div>
+
+    {/* Active Filters for Desktop */}
+    {!isMobile && (
+      <div className="flex gap-2">
+        {filters.map((filter) => (
+          <span key={`${filter.name}-${filter.value}`} className="flex items-center text-[14px]">
+            {filter.value}
+            <button
+              onClick={() => onRemoveFilter(filter)}
+              className="ml-1 text-gray-500 hover:text-red-500"
+            >
+              &times;
+            </button>
+          </span>
+        ))}
+      </div>
+    )}
   </div>
 );
 
