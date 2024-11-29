6c011bd9f816ef40f4ac876d8d56971372491668
DamianKursa <damian.kursa.web@gmail.com>
2024-11-29 20:38:33 +0100

    adding mobile styling

--- src/pages/logowanie.tsx
+++ src/pages/logowanie.tsx
@@ -8,35 +8,33 @@ const Logowanie = () => {
 
   return (
     <Layout title="Logowanie">
-      <div className="bg-[#F9F6F2] flex justify-center items-center min-h-]]]]">
+      <div className="bg-[#F9F6F2] flex justify-center items-center mt-12 mx-4">
         <div className="w-full max-w-4xl bg-white rounded-[40px] shadow-md overflow-hidden transition-all duration-500">
           {/* Row 1 - Login Form */}
           <div
             className={`transition-all duration-500 flex ${
-              isRegistering ? 'h-[150px]' : 'h-[350px]'
-            }`}
+              isRegistering ? 'md:h-[150px]' : 'md:h-[300px]'
+            } flex-col md:flex-row h-auto`}
           >
             {/* Left Section */}
             <div
-              className="p-10 w-1/2"
+              className="p-10 w-full md:w-1/2 flex flex-col justify-center"
               style={{
                 backgroundImage: `url('/images/image-logowanie.png')`, // Replace with the actual path
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
               }}
             >
-              <div className="flex flex-col">
-                <h2 className="text-2xl font-bold mb-4">
-                  Jesteś już użytkownikiem?
-                </h2>
-                <p className="text-sm text-gray-600">
-                  Zaloguj się, aby kontynuować.
-                </p>
-              </div>
+              <h2 className="text-xl md:text-2xl font-bold mb-4 text-center md:text-left">
+                Jesteś już użytkownikiem?
+              </h2>
+              <p className="hidden md:block text-sm text-gray-600">
+                Zaloguj się, aby kontynuować.
+              </p>
             </div>
 
             {/* Right Section */}
-            <div className="w-1/2 p-10 flex flex-col justify-between">
+            <div className="p-10 w-full md:w-1/2 flex flex-col justify-between">
               {!isRegistering && <LoginForm />}
               {isRegistering && (
                 <button
@@ -50,30 +48,28 @@ const Logowanie = () => {
           </div>
 
           {/* Divider */}
-          <div className="px-10">
+          <div className="px-10 md:px-0">
             <div className="h-[1px] bg-gray-300"></div>
           </div>
 
           {/* Row 2 - Register Form */}
           <div
             className={`transition-all duration-500 flex ${
-              isRegistering ? 'h-[500px]' : 'h-[150px]'
-            }`}
+              isRegistering ? 'md:h-[500px]' : 'md:h-[150px]'
+            } flex-col md:flex-row h-auto`}
           >
             {/* Left Section */}
-            <div className="bg-[#CBDBE7] p-10 w-1/2">
-              <div className="flex flex-col">
-                <h2 className="text-2xl font-bold mb-4">
-                  Nie masz jeszcze konta?
-                </h2>
-                <p className="text-sm text-gray-600">
-                  Zarejestruj się, aby cieszyć się zakupami.
-                </p>
-              </div>
+            <div className="bg-[#CBDBE7] p-10 w-full md:w-1/2 flex flex-col justify-center">
+              <h2 className="text-xl md:text-2xl font-bold mb-4 text-center md:text-left">
+                Nie masz jeszcze konta?
+              </h2>
+              <p className="hidden md:block text-sm text-gray-600">
+                Zarejestruj się, aby cieszyć się zakupami.
+              </p>
             </div>
 
             {/* Right Section */}
-            <div className="w-1/2 p-10 flex flex-col justify-between">
+            <div className="p-10 w-full md:w-1/2 flex flex-col justify-between">
               {isRegistering && <RegisterForm />}
               {!isRegistering && (
                 <button
