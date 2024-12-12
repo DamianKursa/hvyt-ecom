e5892da0c1a20af19bccca3e40e7355c1da0d81d
DamianKursa <damian.kursa.web@gmail.com>
2024-12-12 22:05:24 +0100

    adding Hvyt instead Home

--- src/components/UI/Breadcrumbs.component.tsx
+++ src/components/UI/Breadcrumbs.component.tsx
@@ -16,24 +16,34 @@ const Breadcrumbs: React.FC = () => {
     const href = '/' + pathSegments.slice(0, index + 1).join('/');
 
     // Capitalize the first letter of each breadcrumb title
-    const title = segment.replace(/-/g, ' ').replace(/^\w/, (c) => c.toUpperCase());
+    const title = segment
+      .replace(/-/g, ' ')
+      .replace(/^\w/, (c) => c.toUpperCase());
 
     return { href, title };
   });
 
   // Add the home page as the first breadcrumb item
-  breadcrumbItems.unshift({ href: '/', title: 'Home' });
+  breadcrumbItems.unshift({ href: '/', title: 'Hvyt' });
 
   return (
-    <nav aria-label="breadcrumb" className="flex items-center h-10 text-sm text-neutral-darkest">
+    <nav
+      aria-label="breadcrumb"
+      className="flex items-center h-10 text-sm text-neutral-darkest"
+    >
       {breadcrumbItems.map((item, index) => (
         <span key={index} className="flex items-center">
           {index < breadcrumbItems.length - 1 ? (
-            <Link href={item.href} className="hover:underline text-neutral-dark text-[14px] font-light underline">
+            <Link
+              href={item.href}
+              className="hover:underline text-neutral-dark text-[14px] font-light underline"
+            >
               {item.title}
             </Link>
           ) : (
-            <span className="font-light text-neutral-darkest text-[14px]">{item.title}</span>
+            <span className="font-light text-neutral-darkest text-[14px]">
+              {item.title}
+            </span>
           )}
 
           {/* Only show the custom SVG divider if it's not the last item */}
