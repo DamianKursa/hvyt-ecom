d9b69fc55e692f34576e6bb6229bf3a3ab1186ba
DamianKursa <damian.kursa.web@gmail.com>
2025-03-05 08:54:55 +0100

    fixing input spacing

--- src/components/Checkout/CheckoutBillingFrom.tsx
+++ src/components/Checkout/CheckoutBillingFrom.tsx
@@ -168,7 +168,7 @@ const CheckoutBillingForm: React.FC<CheckoutBillingFormProps> = ({
         </label>
       </div>
       {/* Dynamic Fields */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {customerType === 'individual' ? (
           <>
             <input
