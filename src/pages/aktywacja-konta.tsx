0178a2a3eb8018a2063e4318616e23b5a192de4f
DamianKursa <damian.kursa.web@gmail.com>
2025-04-02 07:27:42 +0200

    FIX - styling activation email

--- src/pages/aktywacja-konta.tsx
+++ src/pages/aktywacja-konta.tsx
@@ -4,33 +4,23 @@ import Layout from '@/components/Layout/Layout.component';
 
 const ActivationPage: React.FC = () => {
   const router = useRouter();
-  const { email } = router.query; // Retrieve email from the query string
+  const { email } = router.query;
 
   return (
     <Layout title="Aktywacja konta">
-      <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
-        <div className="bg-white rounded-lg shadow-lg p-8 text-center max-w-md">
-          <h1 className="text-2xl font-bold mb-4">Potwierdź email</h1>
-          <p className="text-sm mb-6">
-            <span className="font-medium text-gray-700"> {email}</span>.
-            <br />W celu zakończenia rejestracji kliknij w link.
-          </p>
-          <img
-            src="/icons/email-icon.svg" // Update with the correct icon path
-            alt="Email Icon"
-            className="mx-auto mb-4"
-          />
-          <h2 className="text-md font-semibold mb-2">Problemy z linkiem?</h2>
-          <p className="text-sm text-gray-600 mb-4">
-            Nie widzisz maila? Sprawdź także w folderze spam.
-            <br />
-            Mail nie dotarł?{' '}
-            <a href="#" className="text-red-500">
-              Wyślij ponownie
-            </a>
-            .
-          </p>
-        </div>
+      <div className="md:mt-0 rounded-[25px] bg-white p-8 shadow-sm flex flex-col items-center justify-center">
+        <img src="/icons/Logo.svg" alt="Logo" className="w-28 h-28 mb-4" />
+        <h1 className="text-[18px] md:text-[28px] font-semibold mb-4 text-black">
+          Potwierdź email
+        </h1>
+        <p className="text-[18px] text-black text-center font-light mb-6">
+          <span className="font-medium text-gray-700">{email}</span>.
+          <br />W celu zakończenia rejestracji kliknij w link.
+        </p>
+        <h2 className="text-md font-semibold mb-2">Problemy z linkiem?</h2>
+        <p className="text-sm text-gray-600 mb-4">
+          Nie widzisz maila? Sprawdź także w folderze spam.
+        </p>
       </div>
     </Layout>
   );
