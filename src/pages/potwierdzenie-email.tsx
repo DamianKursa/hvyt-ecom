1f122c2963d8e00b89f1eb4423ab35229b7bcc4d
DamianKursa <damian.kursa.web@gmail.com>
2025-04-02 07:32:48 +0200

    FIX - styling potwierdzenie-emial

--- src/pages/potwierdzenie-email.tsx
+++ src/pages/potwierdzenie-email.tsx
@@ -5,31 +5,25 @@ import Link from 'next/link';
 
 const PotwierdzeniePage: React.FC = () => {
   const router = useRouter();
-  const { email } = router.query; // Retrieve email from the query string
+  const { email } = router.query; // If needed, you can use this email
 
   return (
-    <Layout title="Potwiedzenie aktywacji email">
-      <div className="flex flex-col items-center justify-center">
-        <div className="bg-white rounded-lg p-8 text-center max-w-md">
-          <h1 className="text-2xl font-bold mb-4">
-            Twoje konto zostało pomyślnie aktywowane!
-          </h1>
-          <img
-            src="/icons/sukces.svg" // Update with the correct icon path
-            alt="Email Icon"
-            className="mx-auto mb-4"
-          />
-          <p className="text-sm mb-6">
-            Dziękujemy za założenie konta. Twoje konto jest teraz aktywne, a Ty
-            możesz w pełni korzystać z wszystkich funkcji naszej platformy.
-          </p>
-          <Link
-            href="/logowanie"
-            className="w-full py-3 px-6 border-2 border-black text-black rounded-full"
-          >
-            Zaloguj się
-          </Link>
-        </div>
+    <Layout title="Potwierdzenie aktywacji email">
+      <div className="md:mt-0 rounded-[25px] bg-white p-8 shadow-sm flex flex-col items-center justify-center">
+        <img src="/icons/sukces.svg" alt="Sukces" className="w-28 h-28 mb-4" />
+        <h1 className="text-[18px] md:text-[28px] font-semibold mb-4 text-black">
+          Twoje konto zostało pomyślnie aktywowane!
+        </h1>
+        <p className="text-[18px] text-black text-center font-light mb-6">
+          Dziękujemy za założenie konta. Twoje konto jest teraz aktywne, a Ty
+          możesz w pełni korzystać z wszystkich funkcji naszej platformy.
+        </p>
+        <Link
+          href="/logowanie"
+          className="w-full py-3 px-6 border-2 border-black text-black rounded-full text-center"
+        >
+          Zaloguj się
+        </Link>
       </div>
     </Layout>
   );
