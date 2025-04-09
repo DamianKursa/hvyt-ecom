49fedeb0ae858d6adb2fbe53f44f0ba569ffb716
DamianKursa <damian.kursa.web@gmail.com>
2025-04-09 15:00:09 +0200

    FIX - adding corect links to Social Links

--- src/components/UI/SocialIcons.tsx
+++ src/components/UI/SocialIcons.tsx
@@ -3,13 +3,25 @@ import Link from 'next/link';
 const SocialIcons = () => {
   return (
     <div className="flex md:justify-end lg:justify-normal space-x-4 mt-0 md:mt-[-20px]">
-      <Link href="#" aria-label="Facebook">
+      <Link
+        target="_blank"
+        href="https://www.facebook.com/HVYTPL/?locale=pl_PL"
+        aria-label="Facebook"
+      >
         <img src="/icons/Facebook.svg" alt="Facebook" className="h-6" />
       </Link>
-      <Link href="#" aria-label="Instagram">
+      <Link
+        target="_blank"
+        href="https://instagram.com/hvyt_pl/"
+        aria-label="Instagram"
+      >
         <img src="/icons/Instagram.svg" alt="Instagram" className="h-6" />
       </Link>
-      <Link href="#" aria-label="Pinterest">
+      <Link
+        target="_blank"
+        href="https://pl.pinterest.com/meethvyt/"
+        aria-label="Pinterest"
+      >
         <img src="/icons/Pinterest.svg" alt="Pinterest" className="h-6" />
       </Link>
     </div>
