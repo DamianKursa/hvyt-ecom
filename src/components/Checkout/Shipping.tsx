532d5f2de0988cf345a93260d0d755b3f84a4387
DamianKursa <damian.kursa.web@gmail.com>
2025-04-14 08:13:21 +0200

    FIX - adding za pobraniem above 300

--- src/components/Checkout/Shipping.tsx
+++ src/components/Checkout/Shipping.tsx
@@ -180,6 +180,7 @@ const Shipping: React.FC<ShippingProps> = ({
                 'kurier gls - darmowa wysyłka',
                 'paczkomaty inpost',
                 'punkty gls',
+                'kurier gls pobranie',
               ].includes(method.title.toLowerCase()),
             );
             // If 'darmowa dostawa' not present, add it
