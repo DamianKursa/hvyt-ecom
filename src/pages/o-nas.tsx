c3fa1521fff95edba293067461c521f2e5fe42ab
Damian Kursa <damian.kursa.web@gmail.com>
2025-07-31 08:50:20 +0200

    FIX - build fix "a"

--- src/pages/o-nas.tsx
+++ src/pages/o-nas.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from '../components/Slider/Slider.component';
 import NaszeKolekcje from '../components/Index/NaszeKolekcje';
 import NewArrivalsSection from '../components/Index/NewArivials.component';
-
+import Link from 'next/link';
 import Layout from '@/components/Layout/Layout.component';
 import HeroOnas from '@/components/Onas/HeroOnas.component';
 import Head from 'next/head';
@@ -23,12 +23,12 @@ const OnasPage = () => {
         <div className="container px-4 md:px-0 mx-auto max-w-grid-desktop">
           <Slider />
           <div className='md:hidden'>
-            <a
+            <Link
               href="/kontakt"
               className="text-center block w-full md:w-auto px-6 py-3 mt-6 md:mt-0 text-black text-lg font-light border border-black rounded-full hover:bg-dark-pastel-red hover:text-neutral-white transition-all"
             >
               Kontakt →
-            </a>
+            </Link>
           </div>
         </div>
       </section>
