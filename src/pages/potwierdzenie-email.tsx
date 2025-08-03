db23d1bce8e6af608077e1f309f234975050a308
Damian Kursa <damian.kursa.web@gmail.com>
2025-08-03 18:52:45 +0200

    FIX - potwierdzenie email

--- src/pages/potwierdzenie-email.tsx
+++ src/pages/potwierdzenie-email.tsx
@@ -8,22 +8,25 @@ const PotwierdzeniePage: React.FC = () => {
   const { email } = router.query;
 
   return (
-    <Layout title="Potwierdzenie aktywacji email">
+    <Layout title="Potwierdź email">
       <div className="md:mt-0 rounded-[25px] bg-white p-8 shadow-sm flex flex-col items-center justify-center">
-        <img src="/icons/sukces.svg" alt="Sukces" className="w-28 h-28 mb-4" />
+        <img
+          src="/icons/email.svg"
+          alt="Potwierdź email"
+          className="w-28 h-28 mb-4"
+        />
         <h1 className="text-[18px] md:text-[28px] font-semibold mb-4 text-black">
-          Twoje konto zostało pomyślnie aktywowane!
+          Potwierdź email
         </h1>
-        <p className="text-[18px] text-black text-center font-light mb-6">
-          Dziękujemy za założenie konta. Twoje konto jest teraz aktywne, a Ty
-          możesz w pełni korzystać z wszystkich funkcji naszej platformy.
+        <p className="text-[18px] text-black text-center font-light mb-2">
+          Link aktywacyjny został wysłany na adres e-mail: {email}
         </p>
-        <Link
-          href="/logowanie"
-          className="w-full py-3 px-6 border-2 border-black text-black rounded-full text-center"
-        >
-          Zaloguj się
-        </Link>
+        <p className="text-[18px] text-black text-center font-light mb-6">
+          W celu zakończenia rejestracji kliknij w link.
+        </p>
+
+        <p className="font-semibold mb-2">Problemy z linkiem?</p>
+        <p className="mb-1">Nie widzisz maila? Sprawdź także w folderze spam.</p>
       </div>
     </Layout>
   );
