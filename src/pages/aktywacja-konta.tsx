94457b6f507c02122f0a55d528a507131f1e019d
Damian Kursa <damian.kursa.web@gmail.com>
2025-08-03 19:07:34 +0200

    FIX - potwierdzenie email

--- src/pages/aktywacja-konta.tsx
+++ src/pages/aktywacja-konta.tsx
@@ -7,20 +7,33 @@ const ActivationPage: React.FC = () => {
   const { email } = router.query;
 
   return (
-    <Layout title="Aktywacja konta">
-      <div className="md:mt-0 rounded-[25px] bg-white p-8 shadow-sm flex flex-col items-center justify-center">
-        <img src="/icons/Logo.svg" alt="Logo" className="w-28 h-28 mb-4" />
-        <h1 className="text-[18px] md:text-[28px] font-semibold mb-4 text-black">
-          Potwierdź email
-        </h1>
-        <p className="text-[18px] text-black text-center font-light mb-6">
-          <span className="font-medium text-gray-700">{email}</span>.
-          <br />W celu zakończenia rejestracji kliknij w link.
-        </p>
-        <h2 className="text-md font-semibold mb-2">Problemy z linkiem?</h2>
-        <p className="text-sm text-gray-600 mb-4">
-          Nie widzisz maila? Sprawdź także w folderze spam.
-        </p>
+    <Layout title="Potwierdź email">
+      <div className="min-h-screen bg-[#F5F5F2] flex items-center justify-center px-4 py-8">
+        <div className="w-[1000px] md:mt-0 rounded-[25px] bg-white p-8 shadow-sm">
+          <div className="max-w-[600px] w-full flex flex-col items-center justify-center mx-auto">
+            <h1 className="text-[18px] md:text-[28px] font-semibold mb-4 text-black">
+              Potwierdź email
+            </h1>
+            <img
+              src="/icons/email-icon.svg"
+              alt="Potwierdź email"
+              className="w-28 h-28 mb-4"
+            />
+
+            <p className="text-[18px] text-black text-center font-light mb-2">
+              Link aktywacyjny został wysłany na adres e-mail: {email}
+            </p>
+            <p className="text-[18px] text-black text-center font-light mb-6">
+              W celu zakończenia rejestracji kliknij w link.
+            </p>
+            <div className="mt-6 space-y-2 self-start">
+              <p className="font-bold">Problemy z linkiem?</p>
+              <p className="text-sm">
+                Nie widzisz maila? Sprawdź także w folderze spam.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
