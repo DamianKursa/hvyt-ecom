acb95576be28c5747532da822f55bdd8dfcd6012
Tomek Klaja <tk@codeq.pl>
2026-02-20 15:10:30 +0100

    tłumaczeni - potwierdzienie email

--- src/pages/potwierdzenie-email.tsx
+++ src/pages/potwierdzenie-email.tsx
@@ -2,31 +2,33 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Layout from '@/components/Layout/Layout.component';
 import Link from 'next/link';
+import { useI18n } from '@/utils/hooks/useI18n';
 
 const PotwierdzeniePage: React.FC = () => {
+  const {t} = useI18n();
   const router = useRouter();
   const { email } = router.query;
 
   return (
-    <Layout title="Potwierdź email">
+    <Layout title={t.account.confirmEmail}>
       <div className="md:mt-0 rounded-[25px] bg-white p-8 shadow-sm flex flex-col items-center justify-center">
         <img
           src="/icons/email.svg"
-          alt="Potwierdź email"
+          alt={t.account.confirmEmail}
           className="w-28 h-28 mb-4"
         />
         <h1 className="text-[18px] md:text-[28px] font-semibold mb-4 text-black">
-          Potwierdź email
+          {t.account.confirmEmail}
         </h1>
         <p className="text-[18px] text-black text-center font-light mb-2">
-          Link aktywacyjny został wysłany na adres e-mail: {email}
+          {t.account.messageEmailLinkSent}: {email}
         </p>
         <p className="text-[18px] text-black text-center font-light mb-6">
-          W celu zakończenia rejestracji kliknij w link.
+          {t.account.messageEmailLinkClick}
         </p>
 
-        <p className="font-semibold mb-2">Problemy z linkiem?</p>
-        <p className="mb-1">Nie widzisz maila? Sprawdź także w folderze spam.</p>
+        <p className="font-semibold mb-2">{t.account.messageLinkProblem}</p>
+        <p className="mb-1">{t.account.messageCheckSpam}</p>
       </div>
     </Layout>
   );
