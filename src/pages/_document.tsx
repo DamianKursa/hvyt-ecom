a30c27823809178c556d4f5de548a8da5b41553b
Tomek Klaja <tk@codeq.pl>
2026-08-11 08:23:13 +0200

    Clarity code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -123,6 +123,13 @@ export default class MyDocument extends Document<MyDocumentProps> {
             })(window,document,'script','dataLayer','${gtmId}');
           `}
           </Script>
+          <Script id="clarity-script" strategy="afterInteractive">
+            {`(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xxj3jhn7q9");`}
+          </Script>
         </Head>
         <body className="bg-beige-light">
           {/* --- Google Tag Manager (noscript) fallback --- */}
